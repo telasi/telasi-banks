df4180fd1b519ad46a93c61e74b9e73095f19b24
Dimitri Kurashvili <dimitri@c12.ge>
2012-06-20 13:16:19 +0400

    adding trash_balance in XML

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ActiveRecord::Base
   set_table_name Telasi::CUSTOMERS_TABLE
   set_primary_key :custkey
   ka_accessor :custname, :accnumb
-  has_one :address, foreign_key: :premisekey
+  has_one  :address, foreign_key: :premisekey
   has_many :pre_payments, class_name: 'Payment', foreign_key: :custkey, conditions: {status: [0,1]}
   has_many :pre_trash_payments, class_name: 'TrashPayment', foreign_key: :custkey, conditions: {status: [0,1]}
 
